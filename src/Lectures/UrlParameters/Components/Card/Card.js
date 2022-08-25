57cb3177047e9a49829f416187d6d3ee2ea8f562
Kevin Ahn <maketheworldwise@gmail.com>
2022-08-25 15:30:17 +0900

    :sparkles: 동적 라우팅
    :fire: 불필요한 import 제거

--- src/Lectures/UrlParameters/Components/Card/Card.js
+++ src/Lectures/UrlParameters/Components/Card/Card.js
@@ -1,13 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Card.scss";
 
 function Card(props) {
+  const navigate = useNavigate();
   return (
-    <div className="cardContainer">
-      <img
-        src={`https://robohash.org/${props.id}?set=set2&size=180x180`}
-        alt=""
-      />
+    <div className="cardContainer" onClick={() => navigate(`/monsters/detail/${props.id}`)}>
+      <img src={`https://robohash.org/${props.id}?set=set2&size=180x180`} alt="" />
       <h2>{props.name}</h2>
       <p>{props.email}</p>
     </div>
--- src/Lectures/UrlParameters/MonsterDetail.js
+++ src/Lectures/UrlParameters/MonsterDetail.js
@@ -1,22 +1,49 @@
-import React, { useState } from "react";
+import { cleanup } from "@testing-library/react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Card from "./Components/Card/Card";
 import "./MonsterDetail.scss";
 
 function MonsterDetail() {
+  const params = useParams();
+  const navigate = useNavigate();
   const [monster, setMonster] = useState({});
 
-  return (
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setMonster(data));
+
+    return () => {
+      cleanup();
+    };
+  }, [params.id]);
+
+  const onPrevClick = () => {
+    navigate(`/monsters/detail/${Number(params.id) - 1}`);
+  };
+
+  const onNextClick = () => {
+    navigate(`/monsters/detail/${Number(params.id) + 1}`);
+  };
+
+  return monster.id ? (
     <div className="urlParameters">
       <div className="btnWrapper">
-        <button>Back to Monsters List</button>
+        <button onClick={() => navigate(`/monsters`)}>Back to Monsters List</button>
       </div>
-      <Card />
+      <Card key={monster.id} id={monster.id} name={monster.name} email={monster.email} />
       <div className="btnWrapper">
-        <button>Previous</button>
-        <button>Next</button>
+        <button onClick={onPrevClick}>Previous</button>
+        <button onClick={onNextClick}>Next</button>
       </div>
     </div>
-  );
+  ) : null;
 }
 
 export default MonsterDetail;
